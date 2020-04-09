5dab106e8f65523b6ea90f018146780eba14a816
Oliver Enes <oliver@enesnet.de>
2020-04-09 19:21:43 +0200

    cleanup

--- untis/create_csv.py
+++ untis/create_csv.py
@@ -3,9 +3,10 @@ import codecs
 
 ENCODING = 'ISO-8859-14'
 
+
 def findName(short, longs):
     for long in longs:
-        if (short == long[0]):
+        if short == long[0]:
             longname = long[1].replace(' ', '').replace('-', '')
             longname = longname.split(';')
             if len(longname) <= 1:
@@ -15,6 +16,7 @@ def findName(short, longs):
             return longname.replace("ä","ae").replace("Ä","Ae").replace("ö","oe").replace("Ö","oe").replace("ü","ue").replace("Ü","ue")
     return ''
 
+
 with codecs.open('GPU004.TXT', 'r', ENCODING) as csvfile:
     with codecs.open('GPU001.TXT', 'r', ENCODING) as coursefile:
         with open('teachers.csv', 'w') as final:
@@ -23,7 +25,6 @@ with codecs.open('GPU004.TXT', 'r', ENCODING) as csvfile:
             li = []
             for course in courses:
                 longname = findName(course[2], names)
-                #print(course[1] + ';' + longname + ';' + course[3] + '\r\n')
                 li.append(course[1] + ';' + longname + ';' + course[3])
             li = list(set(li))
             for l in li:
