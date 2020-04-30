eca54e6c4e89f5c8480d63662a3d9c489ce1cdd5
Oliver Enes <oliver@enesnet.de>
2020-04-30 19:24:16 +0200

    final

--- utils/create_cohorts_from_csv.py
+++ utils/create_cohorts_from_csv.py
@@ -35,54 +35,58 @@ category_12 = m.category_get_id_by_idnumber(category_12_idnumber)
 
 
 def findShort(first, second, ldapusers):
-    first = first.replace("ä", "ae").replace("Ä", "Ae").replace("ö", "oe").replace("Ö", "oe").replace("ü",
-                                                                                                      "ue").replace("Ü",
-                                                                                                                    "ue").replace(
-        'ß', 'ss').replace(' ', '').replace('-', '').replace('von', '')
-    second = second.replace("ä", "ae").replace("Ä", "Ae").replace("ö", "oe").replace("Ö", "oe").replace("ü",
-                                                                                                        "ue").replace(
-        "Ü", "ue").replace('ß', 'ss').replace(' ', '').replace('-', '').replace('von', '')
+    first = first.replace("ä", "ae").replace("Ä", "Ae").replace("ö", "oe").replace("Ö", "oe").replace("ü", "ue").replace("Ü", "ue").replace('ß', 'ss').replace(' ', '').replace('-', '').replace('von', '')
+    second = second.replace("ä", "ae").replace("Ä", "Ae").replace("ö", "oe").replace("Ö", "oe").replace("ü", "ue").replace("Ü", "ue").replace('ß', 'ss').replace(' ', '').replace('-','').replace('von', '')
     for user in ldapusers:
         if user.givenName == first and user.sn == second:
             return user.uid
     return None
 
 
-with codecs.open('0.csv', 'r', ENCODING) as csvfile:
+with codecs.open('export_aus_schulverwaltung.csv', 'r', ENCODING) as csvfile:
     courses = list(csv.reader(csvfile, delimiter=';'))
+    teacherlist = []
     courses.pop(0)
     cs = []
     ccx = []
-    with open('4.csv', "w") as plugin:
-        with open('3.csv', "w") as cohorts:
+    with open('5_Globale_Gruppen_in_Kurse_Plugin.csv', "w") as plugin:
+        with open('4_Nutzer_in_globale_Gruppen.csv', "w") as cohorts:
             cohorts.write('username,cohort1\r\n')
-            with open('2.csv', "w") as cc:
+            with open('3_Globale_Gruppen_anlegen.csv', "w") as cc:
                 cc.write('name,idnumber,description, category\r\n')
-                with open("1.csv", "w") as outfile:
-                    outfile.write("shortname,fullname,category,summary,enrolment_1,enrolment_1_role,enrolment_1_enrolperiod,role_student\r\n")
-                    for course in courses:
-                        teacher_short = course[0][-3:].replace(' ', '').replace('-', '')
-                        shortname = 'TG ' + teacher_short
-                        longname = 'TG ' + course[1]
-                        summary = "Kursmaterial"
-                        student = findShort(course[4], course[3], ldapusers)
-                        if student is None:
-                            print(course[4] + ' ' + course[3] + " not found in ldap")
-                            continue
+                with open('2_Lehrer_einschreiben.csv', "w") as teachers:
+                    teachers.write('username,course1,role1\r\n')
+                    with open("1_Kurse_anlegen.csv", "w") as outfile:
+                        outfile.write("shortname,fullname,category,summary,enrolment_1,enrolment_1_role,enrolment_1_enrolperiod,role_student\r\n")
+                        for course in courses:
+                            teacher_short = course[0][-3:].replace(' ', '').replace('-', '')
+                            shortname = 'TG ' + teacher_short
+                            longname = 'TG ' + course[1]
+                            summary = "TG Angelegenheiten"
+                            student = findShort(course[4], course[3], ldapusers)
+                            if student is None:
+                                print(course[4] + ' ' + course[3] + " notfound in ldap")
+                                continue
 
-                        if course[0][0:2] == "11":
-                            category = category_11
-                        else:
-                            category = category_12
-                        cohorts.write(student + ',' + shortname + '\r\n')
-                        ccx.append(shortname + ',' + shortname + ',TG Angelegenheiten' + ",1\r\n")
+                            teacher_uid = findShort(course[2], course[1], ldapusers)
+                            if teacher_uid is not None:
+                                teacherlist.append(teacher_uid + "," + shortname + ',' + 'editingteacher\r\n')
 
-                        print(shortname, ',', longname, ',', category, ',', summary, ',', 'manual', ',', 'student',
-                              ',,\r\n')
-                        cs.append(shortname + ',' + longname + ',' + str(
-                            category) + ',' + summary + ',' + 'manual' + ',' + 'student' + ',,' + '\r\n')
-                        cs = list(set(cs))
-                        plugin.write(
-                            'add,' + 'cohort,' + shortname + ',' + shortname + ',0,' + shortname + ',student\r\n')
-                    outfile.write(''.join(cs))
-                    cc.write(''.join(list(set(ccx))))
+                            if course[0][0:2] == "11":
+                                category = category_11
+                            else:
+                                category = category_12
+
+                            cohorts.write(student + ',' + shortname + '\r\n')
+                            ccx.append(shortname + ',' + shortname + ',TG Angelegenheiten' + ",1\r\n")
+
+                            print(shortname, ',', longname, ',', category, ',', summary, ',', 'manual', ',', 'student',
+                                  ',,\r\n')
+                            cs.append(shortname + ',' + longname + ',' + str(
+                                category) + ',' + summary + ',' + 'manual' + ',' + 'student' + ',,' + '\r\n')
+                            cs = list(set(cs))
+                            plugin.write(
+                                'add,' + 'cohort,' + shortname + ',' + shortname + ',0,' + shortname + ',student\r\n')
+                        outfile.write(''.join(cs))
+                        teachers.write(''.join(list(set(teacherlist))))
+                        cc.write(''.join(list(set(ccx))))
